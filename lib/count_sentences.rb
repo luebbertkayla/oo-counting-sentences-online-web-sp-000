ac7d21bf081616cb63f7cf7fa10a520c5f8ea15a
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-16 04:26:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,15 +5,18 @@ class String
   def sentence?
     if self.end_with?(".")
       return true 
-  else
-    false
+    else
+      false
     end
   end
   
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      false
+    end
   end
-
   def exclamation?
 
   end
